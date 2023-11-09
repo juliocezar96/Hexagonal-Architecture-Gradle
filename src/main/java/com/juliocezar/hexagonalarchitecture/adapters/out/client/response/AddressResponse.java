5b968f1bcc56a08a5baa1a78ef3e57f03073996c
Julio Cezar <julio.cezar@liferay.com>
2023-11-09 11:34:51 -0300

    Implementation Client microservice addresses moked,

---
+++ src/main/java/com/juliocezar/hexagonalarchitecture/adapters/out/client/response/AddressResponse.java
@@ -0,0 +1,11 @@
+package com.juliocezar.hexagonalarchitecture.adapters.out.client.response;
+
+import lombok.Data;
+
+@Data
+public class AddressResponse {
+
+    private String street;
+    private String city;
+    private String state;
+}
