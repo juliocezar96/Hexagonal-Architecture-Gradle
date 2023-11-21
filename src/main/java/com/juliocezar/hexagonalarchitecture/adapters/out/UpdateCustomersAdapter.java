0a10f444b7f5f036d0930783abcce2cc7abb6324
Julio Cezar <julio.cezar@liferay.com>
2023-11-21 20:58:01 -0300

    UPDATE - Criation adapater

---
+++ src/main/java/com/juliocezar/hexagonalarchitecture/adapters/out/UpdateCustomersAdapter.java
@@ -0,0 +1,24 @@
+package com.juliocezar.hexagonalarchitecture.adapters.out;
+
+import com.juliocezar.hexagonalarchitecture.adapters.out.repository.entity.CustomerRepository;
+import com.juliocezar.hexagonalarchitecture.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.juliocezar.hexagonalarchitecture.application.core.domain.Customer;
+import com.juliocezar.hexagonalarchitecture.application.ports.out.UpdateCustomerOutPutPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCustomersAdapter implements UpdateCustomerOutPutPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity =  customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
